4638e4f53c9f1340f8bc65cbdd304ea185c1a358
Bass Jansson <tech@bassjansson.com>
2023-02-23 19:08:32 +0100

    Adds separate MIDI and DMX tests for easier debugging.

---
+++ src/dmx-test/dmx-test.ino
@@ -0,0 +1,132 @@
+#define ENABLE_SERIAL_DEBUG // Comment this define to disable serial debugging
+
+#include <DmxSimple.h>
+
+#define DMX_TX_PIN   16 // Transmit pin, connect to DI of MAX485 module
+#define DMX_DE_PIN   9  // Enable pin, connect to DE+RE of MAX485 module
+
+#define DMX_NUM_CHAN 24 // Number of DMX channels to use, maximum is 512 (if sufficient RAM)
+
+void setup()
+{
+#ifdef ENABLE_SERIAL_DEBUG
+    // Initialize serial communication
+    Serial.begin(115200);
+    while (!Serial) // Wait for native USB
+        delay(1);
+#endif
+
+    // Setup DMX
+    DmxSimple.usePin(DMX_TX_PIN);
+    DmxSimple.maxChannel(DMX_NUM_CHAN);
+
+    // Enable DMX
+    pinMode(DMX_DE_PIN, OUTPUT);
+    digitalWrite(DMX_DE_PIN, HIGH);
+
+    // Init all DMX channels
+    initDmxChannels();
+} // setup
+
+void loop()
+{
+    // Color test: show each basic color for 2 seconds
+
+    // Turn on RED channels only for 2 seconds
+    writeRgbToDmx(255, 0, 0);
+#ifdef ENABLE_SERIAL_DEBUG
+    Serial.println("RED");
+#endif
+    delay(2000);
+
+    // Turn on GREEN channels only for 2 seconds
+    writeRgbToDmx(0, 255, 0);
+#ifdef ENABLE_SERIAL_DEBUG
+    Serial.println("GREEN");
+#endif
+    delay(2000);
+
+    // Turn on BLUE channels only for 2 seconds
+    writeRgbToDmx(0, 0, 255);
+#ifdef ENABLE_SERIAL_DEBUG
+    Serial.println("BLUE");
+#endif
+    delay(2000);
+
+    // Set lights to MAGENTA for 2 seconds
+    writeRgbToDmx(255, 0, 255);
+#ifdef ENABLE_SERIAL_DEBUG
+    Serial.println("MAGENTA");
+#endif
+    delay(2000);
+
+    // Set lights to YELLOW for 2 seconds
+    writeRgbToDmx(255, 255, 0);
+#ifdef ENABLE_SERIAL_DEBUG
+    Serial.println("YELLOW");
+#endif
+    delay(2000);
+
+    // Set lights to CYAN for 2 seconds
+    writeRgbToDmx(0, 255, 255);
+#ifdef ENABLE_SERIAL_DEBUG
+    Serial.println("CYAN");
+#endif
+    delay(2000);
+}
+
+void initDmxChannels()
+{
+    // Init your lights here.
+    // It might be that some of your lights need some channels
+    // to be on another state than 0 before they turn on at all.
+    // For example, I have some lights which need some channels
+    // to be 255 to turn on.
+
+    // ==> UPDATE THE NUMBERS BELOW WITH YOUR DMX CHANNEL CONFIGURATION! <==
+
+    DmxSimple.write(1, 255);
+    DmxSimple.write(2, 0); // R
+    DmxSimple.write(3, 0); // G
+    DmxSimple.write(4, 0); // B
+    DmxSimple.write(5, 0);
+    DmxSimple.write(6, 0);
+
+    DmxSimple.write(7, 0); // W
+    DmxSimple.write(8, 0);
+
+    DmxSimple.write(9, 0);  // R
+    DmxSimple.write(10, 0); // G
+    DmxSimple.write(11, 0); // B
+    DmxSimple.write(12, 0); // W
+    DmxSimple.write(13, 0); // A
+    DmxSimple.write(14, 0); // UV
+    DmxSimple.write(15, 255);
+    DmxSimple.write(16, 255);
+
+    DmxSimple.write(17, 0); // R
+    DmxSimple.write(18, 0); // G
+    DmxSimple.write(19, 0); // B
+    DmxSimple.write(20, 0); // W
+    DmxSimple.write(21, 0); // A
+    DmxSimple.write(22, 0); // UV
+    DmxSimple.write(23, 255);
+    DmxSimple.write(24, 255);
+}
+
+void writeRgbToDmx(uint8_t r, uint8_t g, uint8_t b)
+{
+    // ==> UPDATE THE NUMBERS BELOW WITH YOUR DMX CHANNEL CONFIGURATION! <==
+
+    DmxSimple.write(2, r); // R
+    DmxSimple.write(3, g); // G
+    DmxSimple.write(4, b); // B
+
+    DmxSimple.write(9, r);  // R
+    DmxSimple.write(10, g); // G
+    DmxSimple.write(11, b); // B
+
+    DmxSimple.write(17, r); // R
+    DmxSimple.write(18, g); // G
+    DmxSimple.write(19, b); // B
+}
---
+++ src/midi-test/midi-test.ino
@@ -0,0 +1,259 @@
+#define ENABLE_SERIAL_DEBUG // Comment this define to disable serial debugging
+
+#define USE_USB_MIDI        // Comment this define to disable use of USB-MIDI
+#define USE_SERIAL_MIDI     // Comment this define to disable use of DIN-5 Serial MIDI
+
+#ifdef USE_USB_MIDI
+    #include <USB-MIDI.h>
+#endif
+#ifdef USE_SERIAL_MIDI
+    #include <MIDI.h>
+#endif
+
+
+#define MIDI_CHANNEL MIDI_CHANNEL_OMNI // MIDI channel to listen to, omni means all channels
+
+
+#ifdef USE_USB_MIDI
+typedef USBMIDI_NAMESPACE::usbMidiTransport  __umt;
+typedef MIDI_NAMESPACE::MidiInterface<__umt> __ss;
+
+__umt usbMIDI(0);                   // cableNr
+__ss  MIDICoreUSB((__umt&)usbMIDI); // USB-MIDI
+#endif
+
+#ifdef USE_SERIAL_MIDI
+MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDICoreSerial); // DIN-5 Serial MIDI
+#endif
+
+
+void setup()
+{
+#ifdef ENABLE_SERIAL_DEBUG
+    // Initialize serial communication
+    Serial.begin(115200);
+    while (!Serial) // Wait for native USB
+        delay(1);
+#endif
+
+#ifdef USE_USB_MIDI
+    // Setup all MIDI event callbacks
+    MIDICoreUSB.setHandleNoteOn(OnNoteOn);
+    MIDICoreUSB.setHandleNoteOff(OnNoteOff);
+    MIDICoreUSB.setHandleAfterTouchPoly(OnAfterTouchPoly);
+    MIDICoreUSB.setHandleControlChange(OnControlChange);
+    MIDICoreUSB.setHandleProgramChange(OnProgramChange);
+    MIDICoreUSB.setHandleAfterTouchChannel(OnAfterTouchChannel);
+    MIDICoreUSB.setHandlePitchBend(OnPitchBend);
+    MIDICoreUSB.setHandleSystemExclusive(OnSystemExclusive);
+    MIDICoreUSB.setHandleTimeCodeQuarterFrame(OnTimeCodeQuarterFrame);
+    MIDICoreUSB.setHandleSongPosition(OnSongPosition);
+    MIDICoreUSB.setHandleSongSelect(OnSongSelect);
+    MIDICoreUSB.setHandleTuneRequest(OnTuneRequest);
+    MIDICoreUSB.setHandleClock(OnClock);
+    MIDICoreUSB.setHandleStart(OnStart);
+    MIDICoreUSB.setHandleContinue(OnContinue);
+    MIDICoreUSB.setHandleStop(OnStop);
+    MIDICoreUSB.setHandleActiveSensing(OnActiveSensing);
+    MIDICoreUSB.setHandleSystemReset(OnSystemReset);
+
+    // Listen for MIDI messages on specified channel
+    MIDICoreUSB.begin(MIDI_CHANNEL);
+#endif
+
+#ifdef USE_SERIAL_MIDI
+    // Setup all MIDI event callbacks
+    MIDICoreSerial.setHandleNoteOn(OnNoteOn);
+    MIDICoreSerial.setHandleNoteOff(OnNoteOff);
+    MIDICoreSerial.setHandleAfterTouchPoly(OnAfterTouchPoly);
+    MIDICoreSerial.setHandleControlChange(OnControlChange);
+    MIDICoreSerial.setHandleProgramChange(OnProgramChange);
+    MIDICoreSerial.setHandleAfterTouchChannel(OnAfterTouchChannel);
+    MIDICoreSerial.setHandlePitchBend(OnPitchBend);
+    MIDICoreSerial.setHandleSystemExclusive(OnSystemExclusive);
+    MIDICoreSerial.setHandleTimeCodeQuarterFrame(OnTimeCodeQuarterFrame);
+    MIDICoreSerial.setHandleSongPosition(OnSongPosition);
+    MIDICoreSerial.setHandleSongSelect(OnSongSelect);
+    MIDICoreSerial.setHandleTuneRequest(OnTuneRequest);
+    MIDICoreSerial.setHandleClock(OnClock);
+    MIDICoreSerial.setHandleStart(OnStart);
+    MIDICoreSerial.setHandleContinue(OnContinue);
+    MIDICoreSerial.setHandleStop(OnStop);
+    MIDICoreSerial.setHandleActiveSensing(OnActiveSensing);
+    MIDICoreSerial.setHandleSystemReset(OnSystemReset);
+
+    // Listen for MIDI messages on specified channel
+    MIDICoreSerial.begin(MIDI_CHANNEL);
+#endif
+} // setup
+
+void loop()
+{
+#ifdef USE_USB_MIDI
+    // Listen to incoming notes
+    MIDICoreUSB.read();
+#endif
+
+#ifdef USE_SERIAL_MIDI
+    // Listen to incoming notes
+    MIDICoreSerial.read();
+#endif
+}
+
+static void OnNoteOn(byte channel, byte note, byte velocity)
+{
+#ifdef ENABLE_SERIAL_DEBUG
+    Serial.print(F("NoteOn  from channel: "));
+    Serial.print(channel);
+    Serial.print(F(", note: "));
+    Serial.print(note);
+    Serial.print(F(", velocity: "));
+    Serial.println(velocity);
+#endif
+}
+
+static void OnNoteOff(byte channel, byte note, byte velocity)
+{
+#ifdef ENABLE_SERIAL_DEBUG
+    Serial.print(F("NoteOff from channel: "));
+    Serial.print(channel);
+    Serial.print(F(", note: "));
+    Serial.print(note);
+    Serial.print(F(", velocity: "));
+    Serial.println(velocity);
+#endif
+}
+
+static void OnAfterTouchPoly(byte channel, byte note, byte pressure)
+{
+#ifdef ENABLE_SERIAL_DEBUG
+    Serial.print(F("AfterTouchPoly from channel: "));
+    Serial.print(channel);
+    Serial.print(F(", note: "));
+    Serial.print(note);
+    Serial.print(F(", pressure: "));
+    Serial.println(pressure);
+#endif
+}
+
+static void OnControlChange(byte channel, byte number, byte value)
+{
+#ifdef ENABLE_SERIAL_DEBUG
+    Serial.print(F("ControlChange from channel: "));
+    Serial.print(channel);
+    Serial.print(F(", number: "));
+    Serial.print(number);
+    Serial.print(F(", value: "));
+    Serial.println(value);
+#endif
+}
+
+static void OnProgramChange(byte channel, byte number)
+{
+#ifdef ENABLE_SERIAL_DEBUG
+    Serial.print(F("ProgramChange from channel: "));
+    Serial.print(channel);
+    Serial.print(F(", number: "));
+    Serial.println(number);
+#endif
+}
+
+static void OnAfterTouchChannel(byte channel, byte pressure)
+{
+#ifdef ENABLE_SERIAL_DEBUG
+    Serial.print(F("AfterTouchChannel from channel: "));
+    Serial.print(channel);
+    Serial.print(F(", pressure: "));
+    Serial.println(pressure);
+#endif
+}
+
+static void OnPitchBend(byte channel, int bend)
+{
+#ifdef ENABLE_SERIAL_DEBUG
+    Serial.print(F("PitchBend from channel: "));
+    Serial.print(channel);
+    Serial.print(F(", bend: "));
+    Serial.println(bend);
+#endif
+}
+
+static void OnSystemExclusive(byte * array, unsigned size)
+{
+#ifdef ENABLE_SERIAL_DEBUG
+    Serial.println(F("SystemExclusive"));
+#endif
+}
+
+static void OnTimeCodeQuarterFrame(byte data)
+{
+#ifdef ENABLE_SERIAL_DEBUG
+    Serial.print(F("TimeCodeQuarterFrame: "));
+    Serial.println(data, HEX);
+#endif
+}
+
+static void OnSongPosition(unsigned beats)
+{
+#ifdef ENABLE_SERIAL_DEBUG
+    Serial.print(F("SongPosition: "));
+    Serial.println(beats);
+#endif
+}
+
+static void OnSongSelect(byte songnumber)
+{
+#ifdef ENABLE_SERIAL_DEBUG
+    Serial.print(F("SongSelect: "));
+    Serial.println(songnumber);
+#endif
+}
+
+static void OnTuneRequest()
+{
+#ifdef ENABLE_SERIAL_DEBUG
+    Serial.println(F("TuneRequest"));
+#endif
+}
+
+static void OnClock()
+{
+#ifdef ENABLE_SERIAL_DEBUG
+    Serial.println(F("Clock"));
+#endif
+}
+
+static void OnStart()
+{
+#ifdef ENABLE_SERIAL_DEBUG
+    Serial.println(F("Start"));
+#endif
+}
+
+static void OnContinue()
+{
+#ifdef ENABLE_SERIAL_DEBUG
+    Serial.println(F("Continue"));
+#endif
+}
+
+static void OnStop()
+{
+#ifdef ENABLE_SERIAL_DEBUG
+    Serial.println(F("Stop"));
+#endif
+}
+
+static void OnActiveSensing()
+{
+#ifdef ENABLE_SERIAL_DEBUG
+    Serial.println(F("ActiveSensing"));
+#endif
+}
+
+static void OnSystemReset()
+{
+#ifdef ENABLE_SERIAL_DEBUG
+    Serial.println(F("SystemReset"));
+#endif
+}
